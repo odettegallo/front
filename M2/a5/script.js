13ea2672e81414141db4af3c15b0072841a75666
Odette <odette.gallo@hotmail.com>
2025-06-18 20:08:50 -0400

    arreglos retroa

--- M2/a5/script.js
+++ M2/a5/script.js
@@ -1,5 +1,6 @@
 function mostrarFecha() {
-const fechain = Date(); 
+// const fechain = Date(); 
+
 const opciones = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
 const hora = {hour: '2-digit'};
 const hr = "horas";
@@ -7,8 +8,8 @@ const minuto = {minute: '2-digit'};
 const min = "minutos";
 const segundo = {second: '2-digit'};
 const seg = "segundos";
-const fecha = new Date(fechain).toLocaleDateString('es-ES', opciones, hora, hr, minuto, min, segundo, seg);
-return fecha, `Hoy es ${fecha} y son las ${new Date().toLocaleTimeString('es-ES', hora)} ${hr}, ${new Date().toLocaleTimeString('es-ES', minuto)} ${min} y ${new Date().toLocaleTimeString('es-ES', segundo)} con ${seg}.`;
+const fecha = new Date().toLocaleDateString('es-ES', opciones, hora, hr, minuto, min, segundo, seg);
+return fecha, `Hoy es ${fecha} y son las ${new Date().toLocaleTimeString('es-ES', hora)} ${hr}, ${new Date().toLocaleTimeString('es-ES', minuto)} ${min} y ${new Date().toLocaleTimeString('es-ES', segundo)} ${seg}.`;
 }
 console.log(mostrarFecha());
 alert(mostrarFecha());
@@ -37,7 +38,7 @@ function countdown() {
 // div.innerHTML = countdown();
 
 document.addEventListener("DOMContentLoaded", function() {
-    let div = document.getElementById("hora");
+    let div = document.getElementById("tiemporestante");
     if (div) {
         div.innerHTML = countdown();
     } else {
