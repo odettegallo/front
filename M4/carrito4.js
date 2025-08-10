de00470e3b45e3cc7a9a66d2273ac482ca8ac6b1
odette gallo <odette gallo@hotmail.com>
2025-08-09 21:37:11 -0400

    carrito4

--- M4/carrito4.js
+++ M4/carrito4.js
@@ -9,6 +9,6 @@ for (i=1;i<10;i++){
     for (j=1;j<10;j++) {  
        console.log(i + " x " + j + ": ")  
        console.log(i*j)  
-       console.log("<br>")  
+       console.log("------------------")  
     }  
 }
