4aea3ec7b8d1f757664d8ab860f4040b17ca6ce0
Odette Gallo <odette.gallo@hotmail.com>
2025-08-12 21:18:31 -0400

    dias

--- M4/n.js
+++ M4/n.js
@@ -7,3 +7,45 @@ function crearArreglo(n) {
 }
 
 console.log(crearArreglo(5)); // [1, 2, 4, 8, 16]
+
+const arregloDiez = [1024, 2, 4, 8, 16, 32, 64, 128, 256, 512];
+
+let mayor = 0;
+for (let i = 0; i < arregloDiez.length; i++) {
+  if (arregloDiez[i] > mayor) {
+    mayor = arregloDiez[i];
+  }
+}
+
+console.log("El número mayor es ",mayor); // 1024
+
+const diasSemana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
+let numeroDia = prompt("Ingrese un número del 1 al 7 para obtener el día de la semana:");
+while (numeroDia < 0 || numeroDia > 7) {
+  alert("Número inválido. Ingrese un número del 1 al 7:");
+  numeroDia = prompt("Ingrese un número del 1 al 7 para obtener el día de la semana:");
+switch (parseInt(numeroDia)) {
+    case 1:
+      prompt("El día es: " + diasSemana[0]);
+      break;
+    case 2:
+      prompt("El día es: " + diasSemana[1]);
+      break;
+    case 3:
+      prompt("El día es: " + diasSemana[2]);
+      break;
+    case 4:
+      prompt("El día es: " + diasSemana[3]);
+      break;
+    case 5:
+      prompt("El día es: " + diasSemana[4]);
+      break;
+    case 6:
+      prompt("El día es: " + diasSemana[5]);
+      break;
+    case 7:
+      prompt("El día es: " + diasSemana[6]);
+      break;
+  }
+}
